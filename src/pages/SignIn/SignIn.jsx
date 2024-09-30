7ee9acdb3bfa819ea88f5d7e2b68d98a0562371f
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-09-30 10:23:25 -0300

    fix: create buttons and navigation

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,10 +1,45 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Background, Container, Logo, AreaInput, Input, SubmiteButton, SubmiteText, Link, LinkText } from "./styles";
+import { Platform } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export function SignIn(){
+
+  const navigation = useNavigation();
+
+  function navegarAtéSignUp(){
+    navigation.navigate('SignUp')
+  }
+
   return(
-    <View>
-      <Text>Tela Login</Text>
-    </View>
+    <Background>
+      <Container
+        behavior={Platform.OS === 'ios' ? 'padding' : ''}
+        enable
+      >
+        <Logo source={require('../../assets/logo.png')} />
+        <AreaInput>
+          <Input 
+            placeholder='Digite seu e-mail'
+          />
+        </AreaInput>
+        <AreaInput>
+          <Input 
+            placeholder='Digite sua senha'
+          />
+        </AreaInput>
+        <SubmiteButton activeOpacity={0.8} >
+          <SubmiteText>
+            Acessar
+          </SubmiteText>
+        </SubmiteButton >
+        <Link activeOpacity={0.4} onPress={navegarAtéSignUp}>
+          <LinkText>
+            Criar uma conta
+          </LinkText>
+        </Link>
+      </Container>
+    </Background>
   )
 }
