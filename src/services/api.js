9be75a78181a247693adfd1697eb7f2eb3ce6362
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-10-01 23:22:50 -0300

    ,,,

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { API_BASE_URL } from "@env";
+import { API_BASE_URL } from "../../@env";
 
 export const api = axios.create({
-    baseURL: API_BASE_URL,
+  baseURL: 'http://10.0.0.165:3333'
 })
