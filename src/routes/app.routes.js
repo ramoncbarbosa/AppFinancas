acd8cf1027b2039e7167aea58a343638b7423c40
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-10-03 10:58:36 -0300

    fragmentacao do drawer routes

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,35 +1,10 @@
-import React, { useContext } from 'react';
-import { createDrawerNavigator, DrawerContentScrollView } from '@react-navigation/drawer';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { Home } from '../pages/Home/Home';
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import { AuthContext } from '../contexts/auth';
+import { Home } from '../pages/Home/Home';
+import { CustomDrawerContent } from '../components/CustomDrawerContent';
 
 const AppDrawer = createDrawerNavigator();
-
-function CustomDrawerContent(props) {
-  const { user, signOut } = useContext(AuthContext);
-
-  return (
-    <DrawerContentScrollView {...props} contentContainerStyle={{ flex: 1 }}>
-      <View style={{ flex: 1, justifyContent: 'space-between' }}>
-        
-        {/* Renderiza o nome do usuário no topo do drawer */}
-        <View style={{ padding: 6, margin: 20, backgroundColor: '#BBB', borderRadius: 4, alignItems: 'center' }}>
-          <Text style={{ fontSize: 16, fontWeight: 'bold', color: '#131313' }}>{user.name}</Text>
-
-        {/* Button de logout*/}
-        </View>
-        <TouchableOpacity
-          onPress={() => signOut()}
-          style={{ padding: 6, margin: 20, backgroundColor: '#f00', borderRadius: 4, alignItems: 'center' }}
-        >
-          <Text style={{ fontSize: 16, fontWeight: 'semibold', color: '#FFF' }}>Sair</Text>
-        </TouchableOpacity>
-      </View>
-    </DrawerContentScrollView>
-  );
-}
 
 export function AppRoutes() {
   return (
---
+++ src/routes/customDrawer.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { DrawerContentScrollView } from '@react-navigation/drawer';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { AuthContext } from '../contexts/auth';
+
+export function CustomDrawerContent(props) {
+  const { user, signOut } = useContext(AuthContext);
+
+  return (
+    <DrawerContentScrollView {...props} contentContainerStyle={{ flex: 1 }}>
+      <View style={{ flex: 1, justifyContent: 'space-between' }}>
+        
+        {/* Renderiza o nome do usuário no topo do drawer */}
+        <View style={{ padding: 6, margin: 20, backgroundColor: '#BBB', borderRadius: 4, alignItems: 'center' }}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold', color: '#131313' }}>{user.name}</Text>
+        </View>
+
+        {/* Botão de logout */}
+        <TouchableOpacity
+          onPress={() => signOut()}
+          style={{ padding: 6, margin: 20, backgroundColor: '#f00', borderRadius: 4, alignItems: 'center' }}
+        >
+          <Text style={{ fontSize: 16, fontWeight: 'semibold', color: '#FFF' }}>Sair</Text>
+        </TouchableOpacity>
+      </View>
+    </DrawerContentScrollView>
+  );
+}
