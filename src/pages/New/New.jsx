2fbc65a5664b51a4c0539a839a1a948763d94ff3
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-10-08 12:06:39 -0300

    fit: criando component de novo registro

---
+++ src/pages/New/New.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Background } from "./styles";
+
+import { Header } from "../../components/Header/Header";
+
+export function New(){
+  return(
+    <Background>
+      <Header title="Registrando"/>
+    </Background>
+  )
+}
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -3,34 +3,38 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import { Home } from '../pages/Home/Home';
 import { CustomDrawer } from './CustomDrawer';
+import { New } from '../pages/New/New';
 
 const AppDrawer = createDrawerNavigator();
 
 export function AppRoutes() {
   return (
     <AppDrawer.Navigator
-    screenOptions={{
-      headerShown: false,
-      drawerStyle: {
-        backgroundColor: '#FFF',
-        paddingTop: 20,
-      },
-      drawerActiveBackgroundColor: '#3B3DBF',
-      drawerActiveTintColor: '#FFF',
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          backgroundColor: '#FFF',
+          paddingTop: 20,
+        },
+        drawerActiveBackgroundColor: '#3B3DBF',
+        drawerActiveTintColor: '#FFF',
 
-      drawerInactiveBackgroundColor: '#F0F2FF',
-      drawerInactiveTintColor: '#121212'
-
-
+        drawerInactiveBackgroundColor: '#F0F2FF',
+        drawerInactiveTintColor: '#121212'
     }}
-
     drawerContent={(props) => <CustomDrawer {...props} />}
    >
+
       <AppDrawer.Screen
         name="Home"
         component={Home}
         
       />
+      <AppDrawer.Screen 
+        name="New"
+        component={New}
+      />
+      
     </AppDrawer.Navigator>
   );
 }
