05bbfb91dcee2a4602233039e8564fdef03ddea2
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-10-08 14:58:12 -0300

    fit: criado metodo de criar receitar e despesas

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -2,14 +2,57 @@ import React, { useState } from "react";
 import { Background, Input, SubmitButton, SubmitText } from "./styles";
 
 import { Header } from "../../components/Header/Header";
-import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
 
 import {RegisterTypes} from '../../components/RegisterTypes/RegisterTypes'
+
+import {api} from '../../services/api.js'
+import {format} from 'date-fns'
 
 export function New(){
   const [labelInput, setLabelInput] = useState('');
   const [valueInput, setValueInput] = useState('');
   const [type, setType] = useState('receita')
+
+  function handleSubmit(){
+    Keyboard.dismiss();
+
+    if(isNaN(parseFloat(valueInput)) || type === null){
+      alert('Preencha todos os campos');
+      return;
+    }
+
+    Alert.alert(
+      'Confirmando dados',
+      `Tipo: ${type}, valor: ${parseFloat(valueInput)}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: "Continuar",
+          onPress: () => handleAdd()
+        }
+      ]
+    )
+  }
+
+  //funcao para cadastrar despesa ou receita
+  async function handleAdd() {
+    Keyboard.dismiss();
+
+    await api.post('/receive', {
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), 'dd/MM/yyyy')
+    })
+
+    setLabelInput("");
+    setValueInput("")
+  }
+
 
   return(
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -33,7 +76,7 @@ export function New(){
             {/* o sendTypechanged e para trocar entre receita e despesa no states no compoentn RegisterTypes */}
             <RegisterTypes type={type} sendTypeChenged={(item) => setType(item)}/>
 
-          <SubmitButton>
+          <SubmitButton onPress={handleSubmit}>
             <SubmitText>
               Registrar
             </SubmitText>
