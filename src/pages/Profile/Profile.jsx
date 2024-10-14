89e89859297f6415de050ee3ef67c56ba52d05a5
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-10-14 19:47:36 -0300

    fix: adjust screan profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Container, Message, Name, NewLink, NewText, LogoutButton, LogoutText } from "./style";
 import { Header } from "../../components/Header/Header";
 import { AuthContext } from "../../contexts/auth";
@@ -7,45 +7,46 @@ import { Avatar } from '@rneui/themed';
 import greetings from '../../services/hi.json'
 
 
-export function Profile(){
-  const {user, signOut} = useContext(AuthContext);
+export function Profile() {
+  const { user, signOut } = useContext(AuthContext);
   const navigation = useNavigation()
 
   //funcao para pegar o nome do user logado e fatiar o mesmo caso seja possivel
-  function getName(name){
+  function getName(name) {
     if (!name) return '';
-    
+
     const nameFatiado = name.split()
     const inicial = nameFatiado.map(partes => partes[0]).join('');
     return inicial.toUpperCase();
   }
 
-  //função para gerar lorem de ois kkkkk
-  function getHis(){
+  //função para pegar as saudações do json
+  function getHis() {
     const his = Math.floor(Math.random() * greetings.greetings.length);
     return greetings.greetings[his]
   }
 
-  
-  return(
+
+  return (
     <Container>
       <Header title="Meu Perfil" />
       <Message>
-       {getHis()}
+        {getHis()}
       </Message>
 
       {/*  de uso da lib externa para avatares */}
       <Avatar
-        size='xlarge'
-        title={getName(user?.name)}
-        overlayContainerStyle={{backgroundColor: '#1E90FF'}}
+        size="xlarge"
+        source={user?.image ? { uri: user.image } : null} 
+        title={getName(user?.name)} 
+        overlayContainerStyle={{ backgroundColor: '#1E90FF' }}
         rounded
       />
       <Name numberOfLines={1}>
         {user && user.name}
       </Name>
 
-      <NewLink onPress={()=> navigation.navigate('Registrar')}>
+      <NewLink onPress={() => navigation.navigate('Registrar')}>
         <NewText>
           Fazer Registro
         </NewText>
